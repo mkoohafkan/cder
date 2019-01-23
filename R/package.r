4b7fad6b97b30631329458dd7f8df4b5cefebe7b
mkoohafkan <michael.koohafkan@gmail.com>
2019-01-23 11:01:22 -0800

    updated package documentation

--- R/package.r
+++ R/package.r
@@ -2,6 +2,17 @@
 #'
 #' Provides CDEC data download capabilities from R.
 #'
+#' @section Package options:
+#'
+#' cder uses the following [options()] to configure behavior:
+#'
+#' \itemize{
+#'   \item `cder.timeout`: The maximum time to wait for a response 
+#'     from the CDEC Web API.
+#' }
+#'
+#' @docType package
+#' @name cder
 "_PACKAGE"
 
 #' @importFrom RCurl getCurlHandle
