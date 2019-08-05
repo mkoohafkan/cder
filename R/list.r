8978d49b90d4b7e3c534df72e04ef74ab23300fe
mkoohafkan <michael.koohafkan@gmail.com>
2019-08-05 08:27:17 -0700

    now using browseURL

--- R/list.r
+++ R/list.r
@@ -1,33 +1,37 @@
+#' CDEC Station Metadata
+#'
+#' Open the Metadata page for a station.
+#' @param station The three-letter station code.
+#' @param ... Additional arguments to pass to `browseURL`.
+#'
+#' @importFrom glue glue
+#' @export
+cdec_meta = function(station, ...) {
+  browseURL(glue("http://cdec.water.ca.gov/dynamicapp/staMeta?station_id={station}"), ...)
+}
+
+
 #' CDEC Search Pages
 #' Open the CDEC station/group search page or map interface.
 #' @name cdec-search
 NULL
 
 #' @describeIn cdec-search CDEC Group search page.
+#' @inheritParams cdec_meta
+#' @importFrom utils browseURL
 #' @export
-cdec_search_groups = function() {
-  shell.exec("http://cdec.water.ca.gov/dynamicapp/queryGroup?s=&get_list=y")
+cdec_search_groups = function(...) {
+  browseURL("http://cdec.water.ca.gov/dynamicapp/queryGroup?s=&get_list=y", ...)
 }
 
 #' @describeIn cdec-search CDEC Station search page.
 #' @export
-cdec_search__stations = function() {
-  shell.exec("http://cdec.water.ca.gov/dynamicapp/staSearch")
+cdec_search_stations = function(...) {
+  browseURL("http://cdec.water.ca.gov/dynamicapp/staSearch", ...)
 }
 
 #' @describeIn cdec-search CDEC Map locator page.
 #' @export
-cdec_map = function() {
-  shell.exec("http://cdec.water.ca.gov/cdecstation2/")
-}
-
-#' CDEC Station Metadata
-#'
-#' Open the Metadata page for a station.
-#' @param station The three-letter station code.
-#'
-#' @importFrom glue glue
-#' @export
-cdec_meta = function(station) {
-  shell.exec(glue("http://cdec.water.ca.gov/dynamicapp/staMeta?station_id={station}"))
+cdec_map = function(...) {
+  browseURL("http://cdec.water.ca.gov/cdecstation2/", ...)
 }
