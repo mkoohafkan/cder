43e48b60899a948cae800048e0645031a6b1b58a
mkoohafkan <michael.koohafkan@gmail.com>
2019-08-07 11:10:32 -0700

    skip tests on CRAN since web connection may not exist

--- tests/testthat/test-list.R
+++ tests/testthat/test-list.R
@@ -1,4 +1,5 @@
 test_that("listings work", {
+  skip_on_cran()
   expect_null(cdec_meta("NSL"))
   expect_null(cdec_search_stations())
   expect_null(cdec_search_groups())
--- tests/testthat/test-query.R
+++ tests/testthat/test-query.R
@@ -1,4 +1,5 @@
 test_that("query works", {
+  skip_on_cran()
   expect_warning(cdec_query("NSL", 100, "E", Sys.Date() - 5, Sys.Date()), NA)
   expect_warning(cdec_query_group("SR1", Sys.Date() - 5, Sys.Date()), NA)
 })
